c84e56e25090848b3deab399d35ba0510662df92
urian <urian1213viera@gmail.com>
2023-02-19 22:04:48 -0500

    fin del desarrollo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,19 @@
-
-
 // Obtiene una lista de todos los elementos del menú
 let menuItems = document.querySelectorAll('.menu-item');
 
 // Agrega un controlador de eventos de clic a cada elemento del menú
-menuItems.forEach(function(item) {
+menuItems.forEach((item)=> {
   item.addEventListener('click', function() {
-    // Elimina la clase "active" de cualquier otro elemento del menú
+
+    /**
+     * Recorro todos los links, y elimino la clase "active" que este en
+     * cualquier otro elemento del menú
+    */
     menuItems.forEach(function(item) {
       item.classList.remove('active');
     });
+
     // Agrega la clase "active" al elemento seleccionado
-    this.classList.add('active');
+    item.classList.add('active');
   });
 });
